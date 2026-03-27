77ba4fbc246c1e298aedaa357a396a314ba6ffcb
JustWain <goshagoog@gmail.com>
2026-03-27 14:01:46 +0500

    AI draft

--- Platform/settings.py
+++ Platform/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_spectacular',
     'corsheaders',
+    'apps.neural_network'
 ]
 
 MIDDLEWARE = [
--- apps/neural_network/views.py
+++ apps/neural_network/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import os
+from dotenv import load_dotenv
+from langchain_gigachat import GigaChat
+from langchain_core.prompts import ChatPromptTemplate
 
-# Create your views here.
+load_dotenv()
+api_key = os.getenv('API_KEY')
+model = GigaChat(
+    credentials = api_key,
+    model = "GigaChat-2",
+    verify_ssl_certs = False,
+    )
+
+def ai_analysis(request):
+    with open('sys_prompt_analysis.txt') as sys_prompt:
+        prompt = ChatPromptTemplate.from_template(f"{sys_prompt}")
+    chain = prompt | model
+
+def ai_filter(request):
+    pass
