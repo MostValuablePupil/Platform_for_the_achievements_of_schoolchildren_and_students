88d653b5835c17ab922d351aeed0f6cf75d04a06
Lisssssssssssssa <mariia.potoskueva@gmail.com>
2026-03-27 23:03:11 +0500

    Add files via upload

---
+++ apps/users/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # 1. Что показываем в общей таблице пользователей 
+    list_display = ("username", "first_name", "last_name", "email", "role", "level", "total_xp")
+    
+    # 2. Фильтры сбоку
+    list_filter = ("role", "level", "is_staff")
+
+    # 3. Карточка РЕДАКТИРОВАНИЯ пользователя (добавляем свой блок с полями)
+    fieldsets = UserAdmin.fieldsets + (
+        ('Профиль и Геймификация', {
+            'fields': ('role', 'level', 'total_xp') 
+        }),
+    )
+
+    # 4. Форма СОЗДАНИЯ нового пользователя (добавляем ФИО и роль при регистрации)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Личные данные', {
+            'fields': ('first_name', 'last_name', 'email', 'role')
+        }),
+    )
