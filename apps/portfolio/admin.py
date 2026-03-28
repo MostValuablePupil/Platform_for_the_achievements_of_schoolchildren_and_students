4d293a3c7fd67da0536f101fe92b38bbdd20534d
Lisssssssssssssa <mariia.potoskueva@gmail.com>
2026-03-28 08:44:46 +0500

    Update admin.py

--- apps/portfolio/admin.py
+++ apps/portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Achievement, Event, Badge, UserBadge
+from .models import Achievement, Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
@@ -10,8 +10,8 @@ class EventAdmin(admin.ModelAdmin):
 
 @admin.register(Achievement)
 class AchievementAdmin(admin.ModelAdmin):
-    list_display = ("title", "student", "event_type", "status", "points", "verifier", "verified_at")
-    list_filter = ("status", "event_type", "verified_at")
+    list_display = ("title", "student", "status", "points", "verifier")
+    list_filter = ("status",)
     search_fields = ("title", "student__username")
 
     
@@ -21,22 +21,3 @@ class AchievementAdmin(admin.ModelAdmin):
             obj.verifier = request.user
             
         super().save_model(request, obj, form, change)
-        
-@admin.register(Badge)
-class BadgeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description') # Колонки в общем списке
-    search_fields = ('name',)              # Поиск по названию бейджа
-
-@admin.register(UserBadge)
-class UserBadgeAdmin(admin.ModelAdmin):
-    # Показываем: Кто получил | Какой бейдж | Когда
-    list_display = ('user', 'badge', 'earned_at') 
-    
-    # Добавляем панель фильтрации сбоку (удобно смотреть всех "Инноваторов")
-    list_filter = ('badge', 'earned_at')
-    
-    # Поиск по логину студента или названию бейджа
-    search_fields = ('user__username', 'badge__name')
-    
-    # Дату получения ставим "только для чтения", чтобы случайно не изменить
-    readonly_fields = ('earned_at',)
