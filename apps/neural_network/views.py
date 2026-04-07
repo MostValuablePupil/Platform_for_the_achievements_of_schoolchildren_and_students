e04df720bcb2edd716770c01b8e3eea738f0f1c8
JustWain <goshagoog@gmail.com>
2026-04-07 11:10:40 +0500

    Analysis and prompt

--- apps/neural_network/views.py
+++ apps/neural_network/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 import os
+import base64
+import requests
+from django.shortcuts import render, get_object_or_404
 from dotenv import load_dotenv
 from langchain_gigachat import GigaChat
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -8,18 +10,58 @@ from apps.portfolio.models import Achievement
 load_dotenv()
 api_key = os.getenv('API_KEY')
 model = GigaChat(
-    credentials = api_key,
-    model = "GigaChat-2",
-    verify_ssl_certs = False,
-    )
+    credentials=api_key,
+    model="GigaChat-2-Lite",
+    verify_ssl_certs=False,
+    temperature=0.1
+)
 
-def ai_analysis(request):
+def ai_analysis(request, achievement_id):
+    achievement = get_object_or_404(Achievement, id=achievement_id)
+    
+    image_data_url = None
+    
+    # Предположим, поле со ссылкой называется 'image_url'
+    img_link = achievement.proof_link 
+    
+    # Если ссылка есть, скачиваем картинку по сети
+    if img_link:
+        try:
+            # Делаем GET-запрос по ссылке с таймаутом, чтобы сервер не завис, если картинка недоступна
+            response = requests.get(img_link, timeout=10)
+            response.raise_for_status() # Бросит ошибку, если ссылка вернет 404 или 500
+            
+            # Берем байты из ответа (response.content) и кодируем в base64
+            encoded_string = base64.b64encode(response.content).decode('utf-8')
+            
+            # Формируем Data-URI
+            image_data_url = f"data:image/jpeg;base64,{encoded_string}"
+            
+        except requests.exceptions.RequestException as e:
+            # Обрабатываем ситуацию, когда картинка по ссылке недоступна
+            print(f"Не удалось скачать картинку по ссылке: {e}")
+
+    # Читаем системный промпт
     with open("apps/neural_network/sys_prompt_analysis.txt") as sys_prompt:
-        messages = [
-            SystemMessage(content=sys_prompt.read()),
-            HumanMessage(content="")
+        sys_content = sys_prompt.read()
+
+    # Формируем контент
+    if image_data_url:
+        human_content = [
+            {"type": "text", "text": ""},
+            {"type": "image_url", "image_url": {"url": image_data_url}}
         ]
-    response = model.invoke(messages)
+    else:
+        raise FileNotFoundError
+
+    messages = [
+        SystemMessage(content=sys_content),
+        HumanMessage(content=human_content)
+    ]
+    
+    # Отправляем запрос в модель
+    ai_response = model.invoke(messages)
+    return ai_response
 
 def ai_filter(request):
     pass
